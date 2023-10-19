b63e71b7345993ab74b9b7ad376df60f99e0cf14
Joshua Pearson <jpearson@ximasoftware.com>
2023-10-19 16:44:30 -0600

    added prettier and eslint

---
+++ .eslintrc.cjs
@@ -0,0 +1,42 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:svelte/recommended",
+    "prettier",
+  ],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: [".eslintrc.{js,cjs}"],
+      parserOptions: {
+        sourceType: "script",
+      },
+    },
+    {
+      files: ["*.svelte"],
+      parser: "svelte-eslint-parser",
+      parserOptions: {
+        parser: {
+          // Specify a parser for each lang.
+          ts: "@typescript-eslint/parser",
+          js: "espree",
+          typescript: "@typescript-eslint/parser",
+        },
+      },
+    },
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["@typescript-eslint"],
+  rules: {},
+};
